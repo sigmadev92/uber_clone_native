8e00692df618f8b0dc0211552a54e025b3a72920
sigmadev92 <devanshofficial2000@gmail.com>
2025-10-18 09:35:33 +0530

    implemented sign-in page. Will implement functionality later on.

--- app/auth/sign_in.tsx
+++ app/auth/sign_in.tsx
@@ -1,13 +1,57 @@
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
 import { Link } from "expo-router";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const onSignInHandler = () => {};
   return (
-    <SafeAreaView className="p-3">
-      <Text>Sign In </Text>
-      <Link href={"/auth/welcome"}>Back</Link>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View>
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="ml-2 font-bold text-2xl">Welcome</Text>
+        </View>
+      </View>
+      <View className="p-5">
+        <InputField
+          label="Email"
+          placeholder="Enter your Email"
+          value={form.email}
+          icon={icons.email}
+          onChangeText={(value) => setForm({ ...form, email: value })}
+        />
+        <InputField
+          label="Password"
+          icon={icons.lock}
+          placeholder="Enter your Password"
+          value={form.password}
+          onChangeText={(value) => setForm({ ...form, password: value })}
+        />
+        <CustomButton
+          title="Sign In"
+          onPress={onSignInHandler}
+          className="mt-3 shadow-black"
+        />
+      </View>
+      {/* OAuth */}
+      <OAuth />
+      <View className="items-center mb-10">
+        <Text className="text-lg">
+          {"Don't"} Have an Account?{" "}
+          <Link href={"/auth/sign_up"} className="text-[red]">
+            Register
+          </Link>
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
 

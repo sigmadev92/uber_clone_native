8cf4709dac529cba6cccf170737f6f9fa7a60415
sigmadev92 <devanshofficial2000@gmail.com>
2025-10-24 16:04:04 +0530

    implemented oAuth

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,26 +1,28 @@
-// import { useOAuth } from "@clerk/clerk-expo";
-// import { router } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { useOAuth } from "@clerk/clerk-expo";
+import { router } from "expo-router";
+import { Alert, Image, Text, View } from "react-native";
 
 import CustomButton from "@/components/CustomButton";
 import { icons } from "@/constants";
-// import { googleOAuth } from "@/lib/auth";
+import { googleOAuth } from "@/lib/auth";
 
 const OAuth = () => {
-  //   const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
 
   const handleGoogleSignIn = async () => {
-    // const result = await googleOAuth(startOAuthFlow);
-    // if (result.code === "session_exists") {
-    //   Alert.alert("Success", "Session exists. Redirecting to home screen.");
-    //   router.replace("/(root)/(tabs)/home");
-    // }
-    // Alert.alert(result.success ? "Success" : "Error", result.message);
+    const result = await googleOAuth(startOAuthFlow);
+    console.log(result);
+    if (result.code === "session_exists") {
+      Alert.alert("Success", "Session exists. Redirecting to home screen.");
+      router.replace("/(root)/(tabs)/home");
+    }
+
+    Alert.alert(result.success ? "Success" : "Error", result.message);
   };
 
   return (
     <View>
-      <View className="flex flex-row justify-center items-center gap-x-3 my-3">
+      <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
         <View className="flex-1 h-[1px] bg-general-100" />
         <Text className="text-lg">Or</Text>
         <View className="flex-1 h-[1px] bg-general-100" />
@@ -28,7 +30,7 @@ const OAuth = () => {
 
       <CustomButton
         title="Log In with Google"
-        className="mb-5 w-[90%] mx-auto shadow-none"
+        className="mt-5 w-full shadow-none"
         IconLeft={() => (
           <Image
             source={icons.google}
